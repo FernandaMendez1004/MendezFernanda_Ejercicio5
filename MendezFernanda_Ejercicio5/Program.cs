52c97cec4db2a15f129b1b7c0d064253e091bbc5
verno <verno@ASUS>
2026-02-22 23:19:28 -0600

    swith implemntado

--- MendezFernanda_Ejercicio5/Program.cs
+++ MendezFernanda_Ejercicio5/Program.cs
@@ -3,16 +3,35 @@ int tipo, historial, antiguedad;
 double ingreso, monto;
 string fiador;
 Console.WriteLine("1 Empleado fijo\r\n2 Temporal\r\n3 Independiente\r\n4 Estudiante");
-Console.Write("Tipo de solicitante: ");
+Console.Write("Tipo de solicitante ");
 tipo = int.Parse(Console.ReadLine());
-Console.Write("Ingreso mensual: ");
+Console.Write("Ingreso mensual");
 ingreso = double.Parse(Console.ReadLine());
-Console.Write("Antiguedad laboral (meses): ");
+Console.Write("Antiguedad laboral  ");
 antiguedad = int.Parse(Console.ReadLine());
-Console.Write("Monto solicitado: ");
+Console.Write("Monto solicitado");
 monto = double.Parse(Console.ReadLine());
-Console.WriteLine("Historial crediticio:");
+Console.WriteLine("Historial crediticio");
 Console.WriteLine("1 Excelente\r\n2 Bueno\r\n3 Regular\r\n4 Malo");
 historial = int.Parse(Console.ReadLine());
-Console.Write("Tiene fiador (S/N): ");
+Console.Write("Tiene fiador s/n ");
 fiador = Console.ReadLine();
+
+switch (tipo)
+{
+    case 1:
+        Console.Write("empleado fijo ");
+        break;
+
+    case 2:
+        Console.Write("temporal ");
+        break;
+
+    case 3:
+        Console.Write("independiente");
+        break;
+
+    case 4:
+        Console.Write("estudiante ");
+        break;
+}
